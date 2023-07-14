d62e1c24162b1b2e9a93b3edec9e092da5c6593b
vatka <48445514+VaTka@users.noreply.github.com>
2023-07-14 19:52:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [passwordStrength, setPasswordStrength] = useState("");
+    const [passwordLabel, setPasswordLabel] = useState("Password");
+    const [passwordInput, setPasswordInput] = useState("");
+    const [passwordColor, setPasswordColor] = useState(["gray", "gray", "gray"]);
+
+    const calculateStrength = (passwordInput) => {
+        if (passwordInput.length < 8) {
+            setPasswordColor(["red", "red", "red"]);
+            return 'Weak';
+        }
+
+        let score = 0;
+
+        if (/^[a-zA-Z0-9!@#$%^&*()]+$/.test(passwordInput)) {
+            score += 0;
+        }
+
+        if (/^(?=.*[a-zA-Z])(?=.*[!@#$%^&*()])|(?=.*[a-zA-Z])(?=.*[0-9])|(?=.*[0-9])(?=.*[!@#$%^&*()])/.test(passwordInput)) {
+            score += 1;
+        }
+
+        if (/^(?=.*[a-zA-Z])(?=.*[!@#$%^&*().,])(?=.*[0-9])/.test(passwordInput)) {
+            score += 1;
+        }
+
+        if (score === 0) {
+            setPasswordColor(["red", "gray", "gray"]);
+            return 'Easy';
+        } else if (score === 1) {
+            setPasswordColor(["yellow", "yellow", "gray"]);
+            return 'Medium';
+        } else {
+            setPasswordColor(["green", "green", "green"]);
+            return 'Strong';
+        }
+    };
+
+    const handlePasswordChange = (e) => {
+        const newPassword = e.target.value;
+        setPasswordInput(newPassword);
+
+        setPasswordLabel(calculateStrength(newPassword));
+        setPasswordInput(passwordStrength);
+    };
+
+    const sectionStyles = passwordColor.map((color) => ({
+        backgroundColor: color,
+    }));
+
+    return (
+        <div className="text-3xl">
+            <input
+                className="bg-gray-200 p-2"
+                onChange={handlePasswordChange}
+            />
+            <div className={`text-${passwordColor[0]}-500`}>{passwordLabel}</div>
+            <div className="flex flex-row">
+                <div className="h-1 w-24" style={sectionStyles[0]}></div>
+                <div className="h-1 w-24" style={sectionStyles[1]}></div>
+                <div className="h-1 w-24" style={sectionStyles[2]}></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
